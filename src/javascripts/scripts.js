3863b6799e306382029d75aa1b441a15298c6009
NA <na.intj@pm.me>
2021-07-13 10:45:45 -0400

    feat: first edits

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -1,2 +1,11 @@
-/* eslint-disable */
-// Add your scripts here
+// Sticky header transition
+var $nav = $(".header-main");
+$nav.toggleClass('atTop')
+
+
+$(function () {
+  $(document).scroll(function () {
+    $nav.toggleClass('isSticky', $(this).scrollTop() > $nav.height());
+	$nav.toggleClass('atTop', $(this).scrollTop() < $nav.height())
+  });
+});
