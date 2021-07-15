92711e2126907709081f333f5d1c56d3a9ead893
NA <na.intj@pm.me>
2021-07-14 23:00:53 -0400

    feat: working on landing page

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -5,10 +5,12 @@ window.$ = $;
 
 // Sticky header transition
 const $nav = $('.header-main');
+const $navImage = $('.navbar-image');
 $nav.toggleClass('atTop');
 
 $(() => {
   $(document).scroll(function () {
+    $navImage.toggleClass('not-visable', $(this).scrollTop() < $nav.height());
     $nav.toggleClass('isSticky', $(this).scrollTop() > $nav.height());
     $nav.toggleClass('atTop', $(this).scrollTop() < $nav.height());
   });
