d628607f61d912e2958a1385632b9ff3d9e86817
NA <na.intj@pm.me>
2021-08-19 19:30:25 -0400

    refactor: compliance check only on buy press

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -87,8 +87,14 @@ $(() => {
 
 // Location Detection
 // Get current token sale data
-// $(() => {
-//   $.getJSON('https://api.db-ip.com/v2/pb6531b8e3bd6e5da1ac7779bc9147d873eddec5/self', (data) => {
-//     if (data.countryCode === 'US') $('#sec-compliance-button')[0].click();
-//   });
-// });
+function secComplianceCheck() {
+  $.getJSON('https://api.db-ip.com/v2/pb6531b8e3bd6e5da1ac7779bc9147d873eddec5/self', (data) => {
+    console.log(data);
+    if (data.countryCode === 'US') $('#sec-compliance-button')[0].click();
+    else $('#how-to-buy-button')[0].click();
+  });
+}
+
+$('[id=buyNow]').on('click', () => {
+  secComplianceCheck();
+});
